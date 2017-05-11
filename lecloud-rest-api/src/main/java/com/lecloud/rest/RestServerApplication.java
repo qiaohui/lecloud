283921e843743eeb64722617ae844355b9ca1272
qiaohui <qiaohui.zhang@gmail.com>
2017-05-11 13:03:14 +0800

    zipkin对服务追踪分析，有性能损耗，接下来考虑spring-cloud-sleuth-zipkin-stream

--- lecloud-rest-api/src/main/java/com/lecloud/rest/RestServerApplication.java
+++ lecloud-rest-api/src/main/java/com/lecloud/rest/RestServerApplication.java
@@ -1,9 +1,16 @@
 package com.lecloud.rest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.context.annotation.Bean;
+
+import zipkin.Span;
 
 /**
  * 通过 EnableDiscoveryClient注解 或 EnableEurekaClient注解，都可启用Eureka客户端用以注册与发现服务，
@@ -17,8 +24,21 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class RestServerApplication {
 
+    private static Log logger = LogFactory.getLog(RestServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(RestServerApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+    public ZipkinSpanReporter spanCollector() {
+        return new ZipkinSpanReporter() {
+            @Override
+            public void report(Span span) {
+                logger.info(span);
+            }
+        };
     }
 
 }
